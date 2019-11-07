537ad1490ca61b11027e3d19789b95644b6d6c9e
Chaim Muskal <monthlyschnitzel@gmail.com>
2019-11-07 04:17:05 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -18,7 +18,8 @@ def third_challenge
    },
    :capulet => {
       :patriarch => {
-        #your key/value pairs here
+        :name => "Lady Capulet",
+        :age "51"
         },
       :matriarch => {
         #your key/value pairs here
